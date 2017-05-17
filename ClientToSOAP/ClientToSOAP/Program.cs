6208f8116e46a32125ad43a0a02b529913a4f9e8
SalvadorGL <gonzalezlsalvador@gmail.com>
2017-05-17 09:02:43 -0700

    Initial upload Soap Client

---
+++ ClientToSOAP/ClientToSOAP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ClientToSOAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("http://www.webservicex.net/CurrencyConvertor.asmx");
+            webRequest.Headers.Add("SOAPAction", "http://www.webservicex.net/CurrencyConvertor.asmx?op=ConversionRate");
+            webRequest.ContentType = "text/xml";
+            webRequest.Method = "POST";
+
+            XmlDocument soapEnvelop = new XmlDocument();
+            string xml = "<?xml version='1.0' encoding='utf - 8'?><soap:Envelope xmlns:xsi = 'http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd = 'http://www.w3.org/2001/XMLSchema' xmlns:soap = 'http://schemas.xmlsoap.org/soap/envelope/' ><soap:Body><ConversionRate xmlns = 'http://www.webserviceX.NET/' ><FromCurrency>USD</FromCurrency><ToCurrency>MXN</ToCurrency></ConversionRate></soap:Body></soap:Envelope>";
+            webRequest.ContentLength = xml.Length;
+            soapEnvelop.LoadXml(xml);
+
+            HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
+            StreamReader responseStream = new StreamReader(webResponse.GetResponseStream());
+
+            Console.WriteLine(responseStream.ReadToEnd());
+            Console.ReadLine();
+
+        }
+    }
+}
